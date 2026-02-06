03f508b8b06d5237493635493939d9c356ee9962
moisesurquisa <moisesurquisasilva@gmail.com>
2026-02-06 16:25:01 -0300

    primeiro commit

---
+++ estudo/views.py
@@ -0,0 +1,5 @@
+from estudo import app
+from flask import render_template
+@app.route('/') #@ significa quue vai ser um decorator, so a / é pq ele vai pegar desde a raiz do app.
+def homepage():#renderizar a pag.
+    return render_template('index.html')
---
+++ main.py
@@ -0,0 +1,4 @@
+from estudo import app
+
+if __name__ == '__main__':#o app so vai ser rodado se o main estiver chamando.
+    app.run(debug=True)# serve para sempre que tiver uma atualizaçao no app ele vai estar estartando a aplicaçao.
