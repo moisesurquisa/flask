e343abdebfb8dd054a8a5f2b2a336a128c209da5
moisesurquisa <moisesurquisasilva@gmail.com>
2026-02-06 16:53:26 -0300

    outra rota

--- estudo/views.py
+++ estudo/views.py
@@ -3,3 +3,7 @@ from flask import render_template
 @app.route('/') #@ significa quue vai ser um decorator, so a / é pq ele vai pegar desde a raiz do app.
 def homepage():#renderizar a pag.
     return render_template('index.html')
+
+@app.route('/nova/') #outra pag'/nova/'
+def novapag():#renderizar a pag.
+    return 'outra view'
