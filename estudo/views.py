e25154e685720362726d296c68dcd1b538faf6af
moisesurquisa <moisesurquisasilva@gmail.com>
2026-02-07 12:32:18 -0300

    uso de render_template e url_for

--- estudo/views.py
+++ estudo/views.py
@@ -1,9 +1,10 @@
 from estudo import app
-from flask import render_template
+from flask import render_template, url_for
 @app.route('/') #@ significa quue vai ser um decorator, so a / é pq ele vai pegar desde a raiz do app.
 def homepage():#renderizar a pag.
     return render_template('index.html')
 
-@app.route('/nova/') #outra pag'/nova/'
-def novapag():#renderizar a pag.
+@app.route('contato') #outra pag'/nova/'
+def nova():#renderizar a pag.
     return 'outra view'
+
